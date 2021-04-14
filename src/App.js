e106f1feefd3f4c0de39bdfd91347143db3cd847
Vitalie <vitalie1998@hotmail.com>
2021-04-14 21:30:43 +0100

    add all the components and link them

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
+import { Router } from "@reach/router";
+import About from "./components/About";
+import Home from "./components/Home";
+import Nav from "./components/Nav";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+
 function App() {
   return (
-    <h1>Hello</h1>
+    <div className="App" >
+      <Nav />
+      <Router>
+        <Home path="/home" />
+        <Projects path="/projects" />
+        <About path="/about" />
+        <Contact path="/contact" />
+      </Router>
+    </div>
   )
 
 }
