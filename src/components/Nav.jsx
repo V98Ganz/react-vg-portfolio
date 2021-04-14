abbeaa7287d582e30a53a4325f2ae137c1817848
Vitalie <vitalie1998@hotmail.com>
2021-04-14 21:20:19 +0100

    change initial path to /home

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,7 +4,7 @@ import { Link } from '@reach/router';
 const Nav = () => {
     return (
         <nav>
-            <Link className="nav-item" to="/" >Home</Link>
+            <Link className="nav-item" to="/home" >Home</Link>
             <Link className="nav-item" to="/projects" >Projects</Link>
             <Link className="nav-item" to="/about" >About</Link>
         </nav>
