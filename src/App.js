d09509a763510fac1a656e9f055352adad249bdc
Vitalie <vitalie1998@hotmail.com>
2021-04-20 21:45:06 +0100

    create all necessary components

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import Home from "./components/Home";
 import Nav from "./components/Nav";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App" >
       <Nav />
       <Router>
-        <Home path="/home" />
+        <Home path="/" />
         <Projects path="/projects" />
         <About path="/about" />
         <Contact path="/contact" />
       </Router>
+      <Footer />
     </div>
   )
 
---
+++ src/components/Projects.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Projects = () => {
+    return (
+        <main>
+            <h1>Projects Page</h1>
+        </main>
+    );
+};
+
+export default Projects;
