a6aee0b9e8c20020eac8175f4fc07675e43c22c1
Vitalie <vitalie1998@hotmail.com>
2021-05-08 14:03:51 +0100

    fix nav

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     <div className="App" >
       <Nav />
       <Router>
-        <Home path="/home" />
+        <Home path="/react-vg-portfolio" />
         <Projects path="/projects" />
         <About path="/about" />
         <Contact path="/contact" />
