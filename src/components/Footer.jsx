b168f13976379da6f63b0f14968ddbf59078bf3c
Vitalie <vitalie1998@hotmail.com>
2021-05-10 16:57:27 +0100

    style bottom half

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,19 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
-    return (
-        <footer>
-            <p className="footer-item" >this is the footer</p>
-            <p className="footer-item" >relevant links</p>
-            <p className="footer-item" >linkedin</p>
-            <p className="footer-item" >github</p>
-        </footer>
-    );
+  return (
+    <footer>
+        <div>
+      <p className="footer-item">
+        “Software is eating the world” - Marc Andreessen
+      </p>
+        </div>
+      <div className="footer-item">
+        <a className="footer-item-a" href="https://www.linkedin.com/in/vitalie-ganziuc-422b9116b/" >Linkedin</a>
+        <a className="footer-item-a" href="https://github.com/V98Ganz" >GitHub</a>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -1,21 +1,38 @@
-import React from 'react';
-import cssIcon from '../img/css-icon.png'
-import knexIcon from '../img/knex-icon.png';
-import htmlIcon from '../img/html-icon.png';
-import jsIcon from '../img/js-icon.png';
-import nodeIcon from '../img/nodejs-icon.png';
+import React from "react";
+import cssIcon from "../img/css-icon.png";
+import knexIcon from "../img/knex-icon.png";
+import htmlIcon from "../img/html-icon.png";
+import jsIcon from "../img/js-icon.png";
+import nodeIcon from "../img/nodejs-icon.png";
 
 const Technologies = () => {
-    return (
-        <section className="home-tech-section" >
-            <p>known Technologies</p>
-            <img src={cssIcon} alt="" />
-            <img src={htmlIcon} alt="" />
-            <img src={knexIcon} alt="" />
-            <img src={jsIcon} alt="" />
-            <img src={nodeIcon} alt="" />
-        </section>
-    );
+  return (
+    <section className="tech" >
+      <h3>Known Technologies</h3>
+      <div className="tech-section">
+        <div className="tech-section-item">
+          <img className="tech-section-image" src={nodeIcon} alt="" />
+          <p>Nodejs</p>
+        </div>
+        <div className="tech-section-item">
+          <img className="tech-section-image" src={jsIcon} alt="" />
+          <p>JavaScript</p>
+        </div>
+        <div className="tech-section-item">
+          <img className="tech-section-image" src={htmlIcon} alt="" />
+          <p>HTML5</p>
+        </div>
+        <div className="tech-section-item">
+          <img className="tech-section-image" src={cssIcon} alt="" />
+          <p>CSS3</p>
+        </div>
+        <div className="tech-section-item">
+          <img className="tech-section-image" src={knexIcon} alt="" />
+          <p>Knex</p>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Technologies;
